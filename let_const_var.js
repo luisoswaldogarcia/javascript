79ddb5528824acc038476984050f049adbb67632
kiketorillo <arellanestorillo@yahoo.com>
2020-05-25 19:29:14 -0500

    nuevo

--- let_const_var.js
+++ let_const_var.js
@@ -1,8 +1,8 @@
 var nombre = "Juan";
 
-
+//@Enrique se declara dentro de la funcion una variable con el mismo nombre pero con un valor diferente el cual solo aplica dentro de esa funcion   
 // declara una nueva variable dentro de la funcion hija
-function saludar(){
+function saludar() {
     var nombre = "Pedro";
     console.log("Hola ", nombre);
 }
@@ -11,11 +11,11 @@ saludar();
 console.log("nombre: ", nombre);
 
 //==============================================================================
-
+//@Enrique  se utiliza la misma variable por lo cual el valor cambia al nuevo dentro de la funcion
 var nombre = "Juan";
 
 //Modufica la variable original
-function saludar(){
+function saludar() {
     nombre = "Pedro";
     console.log("Hola ", nombre);
 }
@@ -24,36 +24,38 @@ saludar();
 console.log("nombre: ", nombre);
 
 //==============================================================================
-
+// let funciona para poder declarar una variable global aunque si se utiliza dentro de una funcion la variable es otra aunque tenga el mismo nombre
 let nombre = "Juan";
 
 //Dentro del bloque de codigo la variable es otra
-function saludar(){
+function saludar() {
     let nombre = "Pedro";
     console.log("Hola ", nombre);
 }
 saludar();
 
 //Dentro del bloque de codigo la variable es otra
-if(nombre == "Juan"){
+if (nombre == "Juan") {
     let nombre = "Jacinto";
     console.log("nombre: ", nombre);
 }
 console.log("despues de if nombre: ", nombre);
 
 //============================================================================== 
+// aunque la variable tenga un valor en el for se inicia en un valor diferente
 var i = 9999999;
 
-for(i=0; i<10; i++){
+for (i = 0; i < 10; i++) {
     console.log("contando: ", i);
 }
 console.log("i: ", i);
 
 //============================================================================== 
+//por el setTimeout se le asigna el valor 5 a i  
 var i = 9999999;
 
-for(i=0; i<5; i++){
-    setTimeout(function(){
+for (i = 0; i < 5; i++) {
+    setTimeout(function() {
         console.log("contando: ", i); //contando: 5
     }, i * 1000);
 }
@@ -64,16 +66,16 @@ let i = 9999999;
 
 // @Brenda: 
 
-// @Enrique: 
+// @Enrique: let funciona como si fueran varias variables
 
-for(let i=0; i<5; i++){
-    setTimeout(function(){
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() {
         console.log("contando: ", i); //contando: 5
     }, i * 1000);
 }
 //=============================================================================
 
 const i = 9999999999;
-i =20;
+i = 20;
 
 console.log("i: ", i);
--- pruebas.js
+++ pruebas.js
@@ -1,7 +1,11 @@
-function saludar(){
+let i = 9999999;
 
-}
+// @Brenda: 
 
-const saludar = function saludar(){
-    
+// @Enrique: let funciona como si fueran varias variables
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log("contando: ", i); //contando: 5
+    }, i * 1000);
 }
